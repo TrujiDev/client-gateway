08449fb6ee967da4cf9c0455d7f767131194f859
TrujiDev <eetgrisales@gmail.com>
2024-05-10 16:32:58 -0500

    Modification in the methods, status paging implemented from the body, creation of DTO's for validation and creation of a new method to modify the status.

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -7,11 +7,14 @@ import {
   Inject,
   ParseUUIDPipe,
   Query,
+  Patch,
 } from '@nestjs/common';
 import { ORDER_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { CreateOrderDto, OrderPaginatioDto } from './dto';
+import { CreateOrderDto, OrderPaginatioDto, StatusDto } from './dto';
 import { catchError } from 'rxjs';
+import { PaginationDto } from 'src/common';
+import { error } from 'console';
 
 @Controller('orders')
 export class OrdersController {
@@ -29,12 +32,43 @@ export class OrdersController {
     return this.ordersClient.send('findAllOrders', orderPaginatioDto);
   }
 
-  @Get(':id')
+  @Get('id/:id')
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.ordersClient.send('findOneOrder', { id }).pipe(
       catchError((error) => {
         throw new RpcException(error);
       }),
     );
+  }
+
+  @Get(':status')
+  getAllByStatus(
+    @Param() statusDto: StatusDto,
+    @Query() paginatioDto: PaginationDto,
+  ) {
+    return this.ordersClient
+      .send('findAllOrders', { ...paginatioDto, status: statusDto.status })
+      .pipe(
+        catchError((error) => {
+          throw new RpcException(error);
+        }),
+      );
+  }
+
+  @Patch(':id')
+  changeOrderStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() statusDto: StatusDto,
+  ) {
+    return this.ordersClient
+      .send('changeOrderStatus', {
+        id,
+        status: statusDto.status,
+      })
+      .pipe(
+        catchError((error) => {
+          throw new RpcException(error);
+        }),
+      );
   }
 }
